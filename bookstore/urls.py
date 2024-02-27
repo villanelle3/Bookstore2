aeafc53c2a88288e12b91412bee38afba161b567
Laura Augusta <laurah.algusta@gmail.com>
2024-02-27 20:43:54 -0300

    pythonanywhere

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -2,7 +2,7 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework.authtoken.views import obtain_auth_token
-
+from bookstore import views
 
 urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
@@ -10,6 +10,7 @@ urlpatterns = [
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+    path("update_server/", views.update, name="update"),
 ]
 
 # 6d2229e19d72aa831844bcb2c3783ee79b1cf482

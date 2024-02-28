7bd8a5a5d28e8c9b6e104e5067ab8d5b5ff9b8ca
Laura <106897289+villanelle3@users.noreply.github.com>
2024-02-27 21:50:22 -0300

    Update urls.py

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -29,20 +29,4 @@ urlpatterns = [
     path('hello/', views.hello_world, name='hello_world'),
 ]
 
-# import debug_toolbar
-# from django.contrib import admin
-# from django.urls import include, path, re_path
-# from rest_framework.authtoken.views import obtain_auth_token
-# from bookstore import views
-
-# urlpatterns = [
-#     path("__debug__/", include(debug_toolbar.urls)),
-#     path("admin/", admin.site.urls),
-#     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
-#     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
-#     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
-#     path("update_server/", views.update, name="update"),
-#     path('hello/', views.hello_world, name='hello_world'),
-# ]
-
 # 6d2229e19d72aa831844bcb2c3783ee79b1cf482
